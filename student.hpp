90ef1d8aba1e2b2e6d2d92676833e557b6a93d58
chenny317 <44509194+chenny317@users.noreply.github.com>
2019-10-10 18:05:38 -0700

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -92,7 +92,7 @@ public:
         void setprovince(string pv);
         //Accessor functions
         string getprovince();
-        friend ostream& opertor <<(ostream& outs, const DomesticStudent& p);
+        friend ostream& operator <<(ostream& outs, const DomesticStudent& p);
 private:
         string province;
 };
